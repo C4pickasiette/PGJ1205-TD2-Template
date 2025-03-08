7808c4b22ec39de123554d8303e88f27f8edb0a5
Marwan Sekkat <marwan.sekkat@gmail.com>
2025-03-08 13:23:03 -0500

    Init Project

---
+++ Source/TD2_Template.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TD2_TemplateTarget : TargetRules
+{
+	public TD2_TemplateTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("TD2_Template");
+	}
+}
---
+++ Source/TD2_Template/TD2_Template.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TD2_Template.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TD2_Template, "TD2_Template" );
